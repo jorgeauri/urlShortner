a23352bf41b541c795141d9bb6a6db2d7f382384
Jorge <jorgeauri@gmail.com>
2021-10-08 18:23:00 -0300

    finished

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+  API_URL: "http://localhost:5000",
+  MONGO_CONNECTION:
+    "mongodb+srv://<username>:<password>@<dbname>.pmpt0.mongodb.net/<dbname>?retryWrites=true&w=majority",
+};
